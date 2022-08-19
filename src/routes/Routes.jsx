f37aa3a3e454db01cd230bce475be4897d8c6ec2
Lucas Moura <93lucasribeiro@gmail.com>
2022-08-19 20:10:07 -0300

    corrigindo conlfito

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
-import SearchBar from '../components/SearchBar';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ SearchBar } />
 
     </Switch>
   );
