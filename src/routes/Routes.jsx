eb85b2148a9d792ab5fe623360d651397d9c204a
Lucas Moura <93lucasribeiro@gmail.com>
2022-08-19 20:04:57 -0300

    ex10 ex11

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
+import SearchBar from '../components/SearchBar';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/foods" component={ SearchBar } />
+
     </Switch>
   );
 }
