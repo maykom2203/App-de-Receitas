5ad7e261d81e150fb45cef1ee23e112dfba8d782
Marcos <njsu5@hotmail.com>
2022-08-22 11:31:41 -0300

    algo deu errado

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -3,12 +3,11 @@ import React from "react";
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from "./helpers/renderWithRouter";
 import Header from "../components/Header";
+import App from "../App";
 
 describe('teste do header', () => {
  it('teste', () => {
 const {history, getByTestId } = renderWithRouter(<Header />)
-
-console.log(history.location.pathname);
 
    const foods = getByTestId('page-title');
    const searchIcon = getByTestId('search-top-btn');
@@ -42,7 +41,43 @@ console.log(history.location.pathname);
    userEvent.type(inpuText, 'cake')
    expect(inpuText).toHaveValue('cake')
    userEvent.click(search);
+  });
+  it('testa /food', () => {
+    const { history, getByTestId } = renderWithRouter(<App />)
 
-   
+    const buttonEnter = getByTestId('login-submit-btn');
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+
+    userEvent.type(emailInput, 'email@email.com');
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(buttonEnter);
+
+    const searchIcon = getByTestId('search-top-btn');
+    userEvent.click(searchIcon);
+
+    const ingerdient = getByTestId('ingredient-search-radio');
+    const name = getByTestId('name-search-radio');
+    const firstLetter = getByTestId('first-letter-search-radio');
+    const inpuText = getByTestId('search-input');
+    const search = getByTestId('exec-search-btn');
+
+    expect(ingerdient).toBeDefined();
+    expect(name).toBeDefined();
+    expect(firstLetter).toBeDefined();
+    expect(inpuText).toBeDefined();
+    expect(search).toBeDefined();
+
+    userEvent.click(firstLetter);
+    expect(firstLetter).toBeChecked()
+    userEvent.click(ingerdient);
+    expect(ingerdient).toBeChecked()
+    userEvent.click(name);
+    expect(name).toBeChecked()
+
+    userEvent.type(inpuText, 'cake')
+    expect(inpuText).toHaveValue('cake')
+    userEvent.click(search);
+
   })
 })
