ea93deefc385ea9de4991703d335d10cdfb839c3
Lucas Moura <93lucasribeiro@gmail.com>
2022-08-22 16:42:20 -0300

    req 14 na tora

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -8,14 +8,29 @@ function Drinks() {
     ({ searchCocktailApi }) => searchCocktailApi.cocktailApi,
   );
   const history = useHistory();
-
+  console.log(storageCocktails);
   if (storageCocktails.length === 1) {
     history.push(`/drinks/${storageCocktails[0].idDrink}`);
   }
+  const maxLength = 12;
   return (
     <div>
       <Route exact path="/drinks" component={ Header } />
       drinks
+      <div>
+        { storageCocktails && storageCocktails.slice(0, maxLength).map((drink, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ drink.idDrink }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ drink.strDrinkThumb }
+              alt={ drink.strDrink }
+            />
+            <p data-testid={ `${index}-card-name` }>
+              {drink.strDrink}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -10,11 +10,26 @@ function Foods() {
   if (storageFoods.length === 1) {
     history.push(`/foods/${storageFoods[0].idMeal}`);
   }
+  const maxLength = 12;
 
   return (
     <div>
       <Route exact path="/foods" component={ Header } />
       Foods
+      <div>
+        { storageFoods && storageFoods.slice(0, maxLength).map((food, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ food.idMeal }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ food.strMealThumb }
+              alt={ food.strMeal }
+            />
+            <p data-testid={ `${index}-card-name` }>
+              {food.strMeal}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
