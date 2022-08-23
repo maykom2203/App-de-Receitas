d447a3063b51f2cdea5688293e45adb502c027ab
Lucas Moura <93lucasribeiro@gmail.com>
2022-08-23 17:30:56 -0300

    lint lint lint

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 function Recipes({ index, recipe }) {
