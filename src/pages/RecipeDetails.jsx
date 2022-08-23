936f791d10eefe8e732b8587f04ece49deedee55
Marcos <njsu5@hotmail.com>
2022-08-23 17:06:54 -0300

    requisito 25

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,17 +1,80 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-// import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+// import YouTube from 'react-youtube';
 import recipeDetailsApi from '../services/recipeDetailsApi';
 
 function RecipeDetails({ match }) {
-  // const [detail, setDetails] = useState({});
+  const [details, setDetails] = useState(null);
   const getApi = async () => {
     const response = await recipeDetailsApi(match.params.id, window.location.pathname);
-    console.log(response);
+    setDetails(response);
   };
-  getApi();
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  // const opts = {
+  //   height: '390',
+  //   width: '640',
+  //   playerVars: {
+  //     ply: details && details.strYoutube,
+  //     autoplay: 1,
+  //   },
+  // };
+
+  const strIngredient = details
+  && Object.keys(details).filter((item) => item.includes('strIngredient'));
+
+  const strMeasure = details
+    && Object.keys(details).filter((item) => item.includes('strMeasure'));
+
   return (
-    <div>{`FoodsId ${match.params.id}`}</div>
+    <div>
+      {details && (
+        <section>
+          <h2 data-testid="recipe-title">
+            {details.strMeal || details.strDrink}
+          </h2>
+          <img
+            src={ details.strMealThumb || details.strDrinkThumb }
+            alt="foto"
+            width="150px"
+            data-testid="recipe-photo"
+          />
+          <p data-testid="recipe-category">
+            {window.location.pathname.includes('/foods')
+              ? details.strCategory
+              : details.strAlcoholic}
+          </p>
+          <div>
+            <h4>ingredientes</h4>
+            {[...strIngredient].map((ing, index) => (
+              details[ing] && (
+                <p
+                  key={ details[ing] }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {details[ing]}
+                  <br />
+                  {details[strMeasure[index]]}
+                </p>
+              )
+            ))}
+          </div>
+          <div>
+            <h4>instruções</h4>
+            <p data-testid="instructions">{details.strInstructions}</p>
+          </div>
+          {/* <p>recomendações</p> */}
+          <p data-testid="0-recomendation-card">recomendações</p>
+          {window.location.pathname.includes('/foods') && (
+            <video data-testid="video">
+              <track kind="captions" src={ details.strYoutube } />
+            </video>)}
+
+        </section>
+      )}
+    </div>
   );
 }
 
--- src/services/recipeDetailsApi.js
+++ src/services/recipeDetailsApi.js
@@ -1,6 +1,7 @@
 const getApi = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
+  console.log(data);
   return data;
 };
 
